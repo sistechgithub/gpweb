9ea0bf76ec39f89598bd9e89ed693cfa7ae40880
jacksondeveloper <jackson.jack.sth@gmail.com>
2016-09-13 08:13:58 -0300

    Criação de componente para seleção em duas listas

---
+++ src/main/webapp/app/components/dualmultiselect/dualmultiselect.js
@@ -0,0 +1,28 @@
+/*
+Created by Alex Klibisz, aklibisz@utk.edu
+February 2015
+*/
+
+var a;
+a = angular.module("dualmultiselect", []), a.directive("dualmultiselect", [function() {
+	return {
+		restrict: 'E',
+		scope: {
+			options: '='
+		},
+		controller: function($scope) {
+			$scope.transfer = function(from, to, index) {
+				if (index >= 0) {
+					to.push(from[index]);
+					from.splice(index, 1);
+				} else {
+					for (var i = 0; i < from.length; i++) {
+						to.push(from[i]);
+					}
+					from.length = 0;
+				}				
+			};
+		},
+		templateUrl: 'app/components/dualmultiselect/dualmultiselect.html'
+	};
+}]);
